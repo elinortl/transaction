2332cc7285385cb64f5ff52963b25dcbe1ba91ad
elinortl <elinorcglr@gmail.com>
2019-06-28 15:24:05 +0300

    wip - modal center

--- src/containers/TransactionDialog.tsx
+++ src/containers/TransactionDialog.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './style.css';
 import { withStyles, createStyles, WithStyles } from '@material-ui/styles';
 import { Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -25,13 +26,33 @@ const styles = (theme: Theme) =>
       marginBottom: 20,
       width: 240,
       height: 50
+    },
+    paper: {
+      alignItems: 'center',
+      alignSelf: 'center',
+      justifyContent: 'center',
+      justifySelf: 'center',
+      flexDirection: 'column',
+      // position: 'absolute',
+      // width: 400,
+      display: 'flex',
+      height: '60%',
+      flex: 1,
+      backgroundColor: 'red',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(4),
+      outline: 'none',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)'
     }
   });
 
 type Props = WithStyles<typeof styles>;
 
 function TransactionDialog({ classes }: Props) {
-  return <div className={classes.root}>test</div>;
+  return <div className={classes.paper}>test</div>;
 }
 
 export default withStyles(styles)(TransactionDialog);
